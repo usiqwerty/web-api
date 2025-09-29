63aaa92f03acd36947bd0e95d0406bdafc096aa0
Alexey404Ivanov <AA.Ivanov2@urfu.me>
2025-09-29 23:47:42 +0500

    Реализовал GET /users

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using WebApi.MinimalApi.Domain;
 using WebApi.MinimalApi.Models;
 using AutoMapper;
+using Newtonsoft.Json;
 
 namespace WebApi.MinimalApi.Controllers;
 
@@ -11,10 +12,13 @@ public class UsersController : Controller
 {
     private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
-    public UsersController(IUserRepository userRepository, IMapper mapper)
+    private readonly LinkGenerator _linkGenerator;
+
+    public UsersController(IUserRepository userRepository, IMapper mapper,  LinkGenerator linkGenerator)
     {
         _userRepository = userRepository;
         _mapper = mapper;
+        _linkGenerator = linkGenerator;
     }
 
     [HttpGet("{userId}")]
@@ -30,6 +34,54 @@ public class UsersController : Controller
         var dto = _mapper.Map<UserDto>(user);
 
         return Ok(dto);
+    }
+    
+    [HttpGet(Name = "GetUsers")]
+    [Produces("application/json", "application/xml")]
+    public IActionResult GetUsers([FromQuery] int pageNumber = 1, [FromQuery] int pageSize = 10)
+    {
+        // Ограничиваем параметры
+        if (pageNumber < 1) pageNumber = 1;
+        if (pageSize < 1) pageSize = 1;
+        if (pageSize > 20) pageSize = 20;
+
+        // Получаем пользователей из репозитория
+        var pageList = _userRepository.GetPage(pageNumber, pageSize);
+
+        var users = _mapper.Map<IEnumerable<UserDto>>(pageList);
+
+        // Считаем инфу для пагинации
+        var totalCount = pageList.TotalCount;
+        var totalPages = (int)Math.Ceiling(totalCount / (double)pageSize);
+
+        // Генерируем ссылки на предыдущую и следующую страницы
+        var previousPageLink = pageNumber > 1
+            ? _linkGenerator.GetUriByRouteValues(
+                HttpContext,
+                "GetUsers",
+                new { pageNumber = pageNumber - 1, pageSize })
+            : null;
+
+        var nextPageLink = pageNumber < totalPages
+            ? _linkGenerator.GetUriByRouteValues(
+                HttpContext,
+                "GetUsers",
+                new { pageNumber = pageNumber + 1, pageSize })
+            : null;
+
+        var paginationHeader = new
+        {
+            previousPageLink,
+            nextPageLink,
+            totalCount,
+            pageSize,
+            currentPage = pageNumber,
+            totalPages
+        };
+
+        Response.Headers.Append("X-Pagination", JsonConvert.SerializeObject(paginationHeader));
+
+        return Ok(users);
     }
 
     [HttpPost]
